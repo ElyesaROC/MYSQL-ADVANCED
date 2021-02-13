0619ca4e5b9204023fb76f533fc04d331a8f7f28
Elyesa_Caglar <elyesa.caglar2002@gmail.com>
2021-02-13 21:32:06 +0100

    insert taak 2 af

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (11, 'Lebron', 'James', '', '', '','1978-07-8', 'Amsterdam');
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (12, 'Kevin', 'Durant', '', '', '','1987-08-20', 'Den Haag');
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (13, 'Magic', 'Johnson', '', '', '','1988-01-06', 'Den Haag');
